d45544c940854894aabd61251d253e2f997e4fed
marine <marine.guillanton@gmail.com>
2017-07-06 19:18:57 -0700

    refactors and improves index structure, animations and style

--- animation.js
+++ animation.js
@@ -1,27 +1,65 @@
+// display side-bar items
+if ($(window).width() > 480) {
+  $(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > 499) {
+      $('.side-transparent .side-header').css({'display': 'none'});
+      $('.side .side-header, .side-menu, .side-footer').css({'display': 'block'});
+      $('.side .side-header, .side-menu, .side-footer').css({'position': 'fixed', 'color': 'white'});
+      $('.side .side-header').css({'top': '0'});
+      $('.side-menu').css({'top': '200px'});
+      $('.side-footer').css({'bottom': '0'});
+    } else {
+      $('.side-transparent .side-header').css({'display': 'block'});
+      $('.side .side-header, .side-menu, .side-footer').css({'display': 'none'});
+    }
+  });
+}
+
+
+$('.link-to-section').on('click', function() {
+  // adds selected state to section in side menu
+  $('.link-to-section').removeClass('selected');
+  $($(this)).addClass('selected');
+  // scrolls to section from side menu
+  $('html, body').animate({
+    scrollTop: $($(this).attr('href')).offset().top
+  }, 600);
+});
+
+
+// shows overley on click
+$('.portfolio-link').on('click', function() {
+  var href = $(this).attr('href');
+  $(href).addClass('visible');
+});
+// hides overlay on click outside from descrition container
+$('.project-overlay').on('click', function() {
+  var id = $(this).attr('id');
+  $('#'+id).removeClass('visible');
+});
+// prevents from hiding overlay on click inside description container
+$(".project-content").on('click', function(event) {
+  event.stopPropagation();
+});
+// hides overlay on close
+$('.close a').on('click', function() {
+  $('body').css({'overflow': 'auto'});
+  var id = $(this).parent().parent().parent().attr('id');
+  $('#'+id).removeClass('visible');
+});
+// prevents background from scrolling when portfolio item is open
+$('.portfolio-item a').on('click', function() {
+  $('body').css({'overflow': 'hidden'});
+});
+
+
 $(window).scroll(function () {
-  $('.brand').css('opacity', 1 - $(window).scrollTop() / 200);
+  // fades out org name on scroll
+  $('.org-name').css('opacity', 1 - $(window).scrollTop() / 200);
+  // fades in transparent side-bar on scroll
   if ($(window).width() > 480) {
     $('.side-transparent').css('display', 'block');
     $('.side-transparent').css('opacity', 0 + $(window).scrollTop() / 500);
   }
 });
-
-if ($(window).width() > 480) {
-  $(window).scroll(function () {
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > 500) {
-      $('.side-footer').css('display', 'block');
-      $('.pictures').css({'position': 'fixed', 'width': '30%', 'top': '0', 'left': '0', 'color': 'white'});
-      $('.side-footer .link').css('color', 'white');
-      $('.copyright').css('color', 'white');
-      $('.side-footer .link').hover(function() {
-        $(this).css('color', 'rgb(20, 164, 40)');
-      }, function() {
-        $(this).css('color', 'white');
-      });
-    } else {
-      $('.side-footer').css('display', 'none');
-      $('.pictures').css({'position': 'absolute', 'width': '100%', 'color': 'black'});
-    }
-  });
-}
